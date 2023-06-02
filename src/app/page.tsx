f09ff6dcb8e1dfc8d22df1ee08e6b94f2a3da5dd
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2023-06-03 01:00:29 +0300

    chore: add counter on main page and modify ui/button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 'use client'
 import styles from './styles/page.module.scss'
-import { PageLayout } from '@/common/layout';
+import { PageLayout } from '@/common/layout'
 import { Header, Footer, Content } from '@layout/components'
 
+// Hooks
+import { useState } from 'react'
+
+// Components
+import Button from '@ui/shared/button'
+
 function Component() {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(pre => pre + 1)
+  }
+
+  function decrement() {
+    setCount(pre => pre > 0 ? pre - 1 : 0)
+  }
+
+  function clearCounter() {
+    setCount(0)
+  }
+
   return <div className={styles.container}>
     <div className={styles.title}>
       next-wrapper
@@ -11,6 +31,27 @@ function Component() {
     <div className={styles.description}>
       Wrapper for next.js 13 version with custom layout and app directory
     </div>
+    <section className={styles.counter}>
+      <p className={styles.count}>{ count }</p>
+      <div className={styles.buttons}>
+        <Button
+          onClick={increment}
+          className={styles.button}
+          content="Increment"
+          type="primary"
+        />
+        <Button
+          onClick={decrement}
+          className={styles.button}
+          content="Decrement"
+        />
+        <Button
+          onClick={clearCounter}
+          className={styles.button}
+          content="Clear"
+        />
+      </div>
+    </section>
   </div>
 }
 
