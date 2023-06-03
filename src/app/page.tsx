6b373ce8a4651427c8c2185fc90a70a0403853ff
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2023-06-03 20:12:05 +0300

    chore: add basic useClientRequest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,23 @@ import { PageLayout } from '@/common/layout'
 import { Header, Footer, Content } from '@layout/components'
 
 // Hooks
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 // Components
 import Button from '@ui/shared/button'
+import { useClientRequest } from '@useClientRequest'
+
+interface ITodo {
+  id: number | string
+  userId: number | string
+  completed: boolean
+  title: string
+}
 
 function Component() {
   const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState<ITodo>()
+  const [request, status] = useClientRequest()
 
   function increment() {
     setCount(pre => pre + 1)
@@ -23,6 +33,16 @@ function Component() {
   function clearCounter() {
     setCount(0)
   }
+
+  useEffect(() => {
+    (async () => {
+      const response: ITodo = await request("https://jsonplaceholder.typicode.com/todos/2")
+
+      if (response.userId) {
+        setTodo(response)
+      }
+    })()
+  }, [request])
 
   return <div className={styles.container}>
     <div className={styles.title}>
---
+++ src/ts/types/shared/index.ts
@@ -0,0 +1,3 @@
+export * from './countries'
+export * from './requestMethods'
+export * from './requestStatus'
